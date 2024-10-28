d25715a2aa52169c102bfb53dd1212d0e747ab9e
khaihtruong <khaihtruong@gmail.com>
2024-10-28 01:16:06 -0400

    Removed unclickable folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, AppBar, Typeography, Grow, Grid} from '@material-ui/core';
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    );
+}
+
+export default App;
